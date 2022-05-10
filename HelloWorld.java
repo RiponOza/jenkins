b5758c59d6d41e02c338efc4c01b5b8c723846c5
Ripon Oza <39427975+RiponOza@users.noreply.github.com>
2022-05-10 18:06:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("**********Hello Jenkins Git Poll-SCM**********");
+        System.out.println("**********Hello Jenkins Git Poll-SCM Edited_01**********");
     }
 }
