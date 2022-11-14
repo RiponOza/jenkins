6fb0b354115d5f36d570e39ec6d4020f1a3754ad
Ripon Oza <39427975+RiponOza@users.noreply.github.com>
2022-11-14 15:15:43 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
+import sys
+
+first_name = (sys.argv[1]).capitalize()
+last_name = (sys.argv[2]).capitalize()
+
 print("Hello World from python 3 !")
+print("Hi, {} {}. Hope you are learning Jenkins.".format(first_name, last_name))
